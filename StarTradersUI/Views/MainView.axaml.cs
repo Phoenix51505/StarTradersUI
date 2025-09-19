06b4a49034314c8a3628fc218a3c195e97a71f30
Phoenix51505 <88960080+Phoenix51505@users.noreply.github.com>
2025-09-19 11:47:22 -0400

    Log in UI

--- StarTradersUI/Views/MainView.axaml.cs
+++ StarTradersUI/Views/MainView.axaml.cs
@@ -1,5 +1,7 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
 using System;
+using System.Diagnostics;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -10,6 +12,29 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
-        
+
+    }
+    private void SwitchEvent(object? sender, RoutedEventArgs e)
+    {
+        SignLabel.IsVisible = !SignLabel.IsVisible;
+        SignBox.IsVisible = !SignBox.IsVisible;
+        SignButton.IsVisible = !SignButton.IsVisible;
+        SignSwitch.IsVisible = !SignSwitch.IsVisible;
+        RegisterLabel.IsVisible = !RegisterLabel.IsVisible;
+        RegisterAccountBox.IsVisible = !RegisterAccountBox.IsVisible;
+        RegisterSymbolBox.IsVisible = !RegisterSymbolBox.IsVisible;
+        RegisterComboBox.IsVisible = !RegisterComboBox.IsVisible;
+        RegisterButton.IsVisible = !RegisterButton.IsVisible;
+        RegisterSwitch.IsVisible = !RegisterSwitch.IsVisible;
+        if (SignLabel.IsVisible == true)
+        {
+            Entry.Height = 156;
+            EntryBorder.Height = 176;
+        }
+        else
+        {
+            Entry.Height = 228;
+            EntryBorder.Height = 248;
+        }
     }
 }
