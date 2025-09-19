5fa0c3eccece4da8ea2edd01994936eada434f5f
Phoenix51505 <88960080+Phoenix51505@users.noreply.github.com>
2025-09-19 04:52:08 -0400

    API stuffs

--- StarTradersUI/Views/MainWindow.axaml.cs
+++ StarTradersUI/Views/MainWindow.axaml.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Net.Http;
 using System.Text.Json;
-using System.Threading.Tasks;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
-
+using StarTradersUI.Api;
 namespace StarTradersUI.Views;
 
 public partial class MainWindow : Window
@@ -21,9 +20,9 @@ public partial class MainWindow : Window
                 Method = HttpMethod.Get,
                 RequestUri = new Uri("https://api.spacetraders.io/v2/factions?limit=20"),
             };
-        using var response = await GlobalStates.client.SendAsync(request);
-        response.EnsureSuccessStatusCode();
-        var factionList = await response.Content.ReadAsStringAsync();
+            using var response = await GlobalStates.client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            var factionList = await response.Content.ReadAsStringAsync();
         }
     }
 }
