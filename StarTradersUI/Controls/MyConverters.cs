5f0f6c8b7c718a90aa349c089023e849cf33172b
Lexi <arallen3660@gmail.com>
2025-09-20 13:31:43 -0400

    Make minimum root window width be 1.5x its height

---
+++ StarTradersUI/Controls/MyConverters.cs
@@ -0,0 +1,8 @@
+﻿using Avalonia.Data.Converters;
+
+namespace StarTradersUI.Controls;
+
+public static class MyConverters
+{
+    public static FuncValueConverter<double?, double?> WindowSizeConverter { get; } = new(v => v * 1.5);
+}
