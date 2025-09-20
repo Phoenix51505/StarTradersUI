2803a6da3e6460178c6aead0e961fafcc3e78a35
Lexi <arallen3660@gmail.com>
2025-09-20 07:35:55 -0400

    Working space map ui, will add some selection callbacks

--- StarTradersUI/Views/MainView.axaml.cs
+++ StarTradersUI/Views/MainView.axaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Avalonia.Interactivity;
 using Avalonia.Threading;
 
 namespace StarTradersUI.Views;
@@ -13,5 +14,29 @@ public partial class MainView : UserControl
     {
         InitializeComponent();
         Dispatcher.UIThread.InvokeAsync(GlobalStates.InitializeGlobalState);
+    }
+    
+    private void SwitchEvent(object? sender, RoutedEventArgs e)
+    {
+        SignLabel.IsVisible = !SignLabel.IsVisible;
+        SignBox.IsVisible = !SignBox.IsVisible;
+        SignButton.IsVisible = !SignButton.IsVisible;
+        SignSwitch.IsVisible = !SignSwitch.IsVisible;
+        RegisterLabel.IsVisible = !RegisterLabel.IsVisible;
+        RegisterAccountBox.IsVisible = !RegisterAccountBox.IsVisible;
+        RegisterSymbolBox.IsVisible = !RegisterSymbolBox.IsVisible;
+        RegisterComboBox.IsVisible = !RegisterComboBox.IsVisible;
+        RegisterButton.IsVisible = !RegisterButton.IsVisible;
+        RegisterSwitch.IsVisible = !RegisterSwitch.IsVisible;
+        if (SignLabel.IsVisible)
+        {
+            Entry.Height = 156;
+            EntryBorder.Height = 176;
+        }
+        else
+        {
+            Entry.Height = 228;
+            EntryBorder.Height = 248;
+        }
     }
 }
--- StarTradersUI/Views/MainWindow.axaml.cs
+++ StarTradersUI/Views/MainWindow.axaml.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Net.Http;
 using System.Text.Json;
+using System.Threading.Tasks;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using StarTradersUI;
@@ -15,15 +16,6 @@ public partial class MainWindow : Window
     }
     private async void Window_Loaded(object sender, RoutedEventArgs e)
     {
-        /*{
-            var request = new HttpRequestMessage
-            {
-                Method = HttpMethod.Get,
-                RequestUri = new Uri("https://api.spacetraders.io/v2/factions?limit=20"),
-            };
-            using var response = await GlobalStates.client.SendAsync(request);
-            response.EnsureSuccessStatusCode();
-            var factionList = await response.Content.ReadAsStringAsync();
-        }*/
+        await GlobalStates.InitializeGlobalState();
     }
 }
